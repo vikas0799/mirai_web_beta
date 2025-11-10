6777931fbc595bfadba9e4a6ed88e358d5187312
Vikas Patel <vikaspatel@Vikass-MacBook-Air.local>
2025-11-10 21:50:40 +0530

    js basics

---
+++ Lect-24-js-basics/app.js
@@ -0,0 +1,20 @@
+// console.log("hello mirai !");
+
+// var a=10
+// console.log(a)
+
+// a="shreya";
+// console.log(a);
+// console.log(typeof(a))
+
+// alert("enter otp ")
+
+
+// document.write("hello mirai")
+
+let a=10;
+let b=20;
+
+
+let sum=a+b;
+console.log(sum);
---
+++ Lect-24-js-basics/prime.js
@@ -0,0 +1,17 @@
+let num=49;
+
+let temp=true;
+for(let i=2;i<=num-1;i++){
+    if(num%i==0){
+        temp=false;
+    }
+}
+
+if(temp==true){
+    console.log("it is prime");
+    
+}
+else{
+    console.log("Not prime");
+    
+}
